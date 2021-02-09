c5f366c6121a5a8ecf5bbed3380e7a0ca2153b94
Siddharth Patel <spatel8266@conestogac.on.ca>
2021-02-09 16:15:14 -0500

    Containing the completed Rectangle class

---
+++ Assignment1PROG8170/RectConsole.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Assignment1PROG8170
+{
+    public class RectConsole
+    {
+
+        static void Main(string[] args)
+        {
+            int length, width;
+            Rectangle rt = new Rectangle();
+            Console.WriteLine("Enter length");
+            length = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Width");
+            width = Convert.ToInt32(Console.ReadLine());
+            rt.SetLength(length);
+            rt.SetWidth(width);
+
+            Console.WriteLine("\nPerimeter of rectangle: " + rt.GetPerimeter());
+            Console.WriteLine("\nArea of rectangle: " + rt.GetArea());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Assignment1PROG8170/Rectangle.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment1PROG8170
+{
+   public class Rectangle
+    {
+        private int length, width;
+       public Rectangle()
+        {
+            length = 1;
+            width = 1;
+        }
+        Rectangle(int length, int width)
+        {
+            this.length = length;
+            this.width = width;
+
+        }
+        public int GetLength()
+        {
+            return length;
+        }
+        public int SetLength(int length)
+        {
+            return this.length = length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int SetWidth(int width)
+        {
+            return this.width = width;
+        }
+        public int GetPerimeter()
+        {
+
+            return 2 * (length + width);
+
+        }
+        public int GetArea()
+        {
+            int area = length * width;
+            return area;
+        }
+    }
+}
